b50632f9ddde145877c0a72522d4b1b486b516b4
Ivanrm01 <correo@irmabogados.es>
2026-08-12 13:46:21 +0200

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,7 @@ const links = [
   { href: '/', label: 'Inicio' },
   { href: '/sobre-nosotros', label: 'Sobre nosotros' },
   { href: '/servicios', label: 'Servicios' },
+  { href: '/derivaciones-responsabilidad-tributaria', label: 'Derivaciones' },
   { href: '/garantias-deuda-aeat', label: 'Garantías' },
   { href: '/blog', label: 'Blog' },
   { href: '/contacto', label: 'Contacto' },
